b885d0573e545671fc7eeca989c91ffc37084f12
imageplus <admin@image-plus.co.uk>
2021-09-22 10:23:39 +0100

    Run init first if we're on cli

--- s3-uploads.php
+++ s3-uploads.php
@@ -13,5 +13,7 @@ require_once __DIR__ . '/inc/namespace.php';
 add_action( 'plugins_loaded', 'S3_Uploads\\init', 0 );
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	S3_Uploads\init(); //needs to be initialised on the cli as the plugins aren't loaded before this command is registered
+	
 	WP_CLI::add_command( 's3-uploads', 'S3_Uploads\\WP_CLI_Command' );
 }
